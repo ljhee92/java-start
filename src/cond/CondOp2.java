7e85016cf290396745ab1c7fb690cb0adeba3bb3
이주희 <juhee@juhee.local>
2023-12-18 22:08:40 +0900

    Initial commit

---
+++ src/cond/CondOp2.java
@@ -0,0 +1,11 @@
+package cond;
+
+public class CondOp2 {
+    public static void main(String[] args) {
+
+        // 삼항연산자 ?:
+        int age = 16;
+        String status = (age >= 18) ? "성인" : "미성년자";
+        System.out.println("age = " + age + " status = " + status);
+    }
+}
