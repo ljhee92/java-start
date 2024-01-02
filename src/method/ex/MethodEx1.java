4d6d0b50c3fb57d61f670720fcbfeff640fe8d86
juhee <weilainib7@gmail.com>
2024-01-03 00:14:36 +0900

    method ex

--- src/method/ex/MethodEx1.java
+++ src/method/ex/MethodEx1.java
@@ -2,17 +2,12 @@ package method.ex;
 
 public class MethodEx1 {
     public static void main(String[] args) {
-
-        double average1 = average(1, 2, 3);
-        System.out.println("평균값: " + average1);
-
-        double average2 = average(15, 25, 35);
-        System.out.println("평균값: " + average2);
+        System.out.println("평균값: " + average(1,2,3));
+        System.out.println("평균값: " + average(15, 25, 35));
     }
 
     public static double average(int a, int b, int c) {
         int sum = a + b + c;
-        double average = sum / 3.0;
-        return average;
+        return sum / 3.0;
     }
 }
